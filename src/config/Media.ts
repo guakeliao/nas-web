90711ee44ee0aa69bf24b2c21b76df90a546a0ed
liaowei3 <liaowei3@asiainfo.com>
2022-11-15 12:02:34 +0800

    优化

--- src/config/Media.ts
+++ src/config/Media.ts
@@ -26,6 +26,7 @@ const medias: Array<Media> = [
     new Media('青龙1', '5701', '账号池1'),
     new Media('青龙2', '5702', '账号池2'),
     new Media('青龙_其他', '5709', '账号池9'),
+    new Media('下载器', '9091', 'tr下载器'),
     new Media('更新jd', '--', '更新ck', false),
 ]
 export default medias
